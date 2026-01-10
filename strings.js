622d556c97d21f94c5f45b7886f9b5adaba2604b
aakarshakverma4377 <aakarshakverma4377@gmail.com>
2026-01-10 14:59:05 +0530

    strings lecture

---
+++ strings.js
@@ -0,0 +1,18 @@
+let name = "zeus"
+// console.log(name.length)
+console.log(name[0])
+// let fri = 'hello'
+// console.log(fri.length)
+
+// template literals
+
+let boy1 = "Pramod"
+let boy2 = "Nikhil"
+// Nikhil is a friend of Pramod
+let sentence = `${boy2} is a friend of ${boy1}`
+console.log(sentence)
+
+// escape sequence characters
+
+let fruit = 'bana\rna'
+console.log(fruit)
