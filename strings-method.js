9dacc5607ee120353263773c764fda769d5b6eec
aakarshakverma4377 <aakarshakverma4377@gmail.com>
2026-01-10 20:31:14 +0530

    string methods

---
+++ strings-method.js
@@ -0,0 +1,18 @@
+// let name = "Zeus"
+// console.log(name.length)
+
+//console.log(name.toUpperCase())
+//console.log(name.toLowerCase())
+//console.log(name.slice(2,12))
+//console.log(name.slice(2))
+//console.log(name.replace("scorpio", "999"))
+
+//let friend = "Jaiger"
+//console.log(name.concat(" is a friend of ", friend, " Best"
+
+//Practice Set
+
+let string = "aakarshakverma"
+for (let i = 0; i < string.length; i++) {
+    console.log(string[i]);
+}
